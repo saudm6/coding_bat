41c14cde7ec54ebfa8f6bf65aa9c3ab6b7373bf3
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-25 18:22:33 +0400

    Completed startOz

---
+++ startOz.java
@@ -0,0 +1,12 @@
+public String startOz(String str) {
+    if (str.startsWith("oz")){
+        return "oz";
+    }
+    else if (str.startsWith("o")){
+        return "o";
+    }
+    else if (str.startsWith("z", 1)){
+        return "z";
+    }
+    return "";
+}
