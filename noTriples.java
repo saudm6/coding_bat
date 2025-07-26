a9c07df95357e44d87e88cf26f9862d04d69302f
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-26 23:31:29 +0400

    Completed noTriples

--- noTriples.java
+++ noTriples.java
@@ -1,2 +1,10 @@
-public class noTriples {
+public boolean noTriples(int[] nums) {
+    for (int i = 0; i < nums.length - 2; i++){
+        if (nums[i] == nums[i+1]){
+            if (nums[i+1] == nums[i+2]){
+                return false;
+            }
+        }
+    }
+    return true;
 }
