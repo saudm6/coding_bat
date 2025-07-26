df1a1a379031623e82e19485855d846a4aafa84a
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-26 17:34:32 +0400

    Completed stringMatch

---
+++ stringMatch.java
@@ -0,0 +1,11 @@
+public int stringMatch(String a, String b) {
+    int count = 0;
+    int total_length = Math.min(a.length(), b.length());
+
+    for (int i = 0; i < total_length - 1; i++){
+        if (a.substring(i, i + 2).equals(b.substring(i, i + 2))){
+            count++;
+        }
+    }
+    return count;
+}
