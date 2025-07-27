eae10bc61d4bcf720234115f60ad0a2e54898939
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-27 17:36:22 +0400

    Completed minCat

---
+++ minCat.java
@@ -0,0 +1,10 @@
+public String minCat(String a, String b) {
+    if (a.length() > b.length()){
+        String longer = a;
+        return a.substring(a.length() - b.length()) + b;
+    }
+    else if (b.length() > a.length()){
+        return a + b.substring(b.length() - a.length());
+    }
+    return a + b;
+}
