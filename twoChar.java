4e811fa6939fdb49f6c406f73ef1d63a83d43cbd
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-27 14:14:25 +0400

    Completed twoChar

---
+++ twoChar.java
@@ -0,0 +1,6 @@
+public String twoChar(String str, int index) {
+    if (index > str.length() -2 || index < 0){
+        return str.substring(0, 2);
+    }
+    return str.substring(index, index+2);
+}
