35d1e281b74ad19b2708a87391bf143aab8ee5f8
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-28 21:46:25 +0400

    Completed make2

---
+++ make2.java
@@ -0,0 +1,17 @@
+public int[] make2(int[] a, int[] b) {
+
+    int[] arr = new int[2];
+
+    if (a.length >= 2){
+        arr[0] = a[0];
+        arr[1] = a[1];
+        return arr;
+    }
+    if (a.length == 1){
+        arr[0] = a[0];
+        arr[1] = b[0];
+        return arr;
+    }
+
+    return b;
+}
