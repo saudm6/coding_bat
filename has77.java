56ee3ef0e6e4d2a59ff705fb9295112efa3960fd
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-31 21:05:52 +0400

    Completed has77

---
+++ has77.java
@@ -0,0 +1,13 @@
+public boolean has77(int[] nums) {
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 7) {
+            if (i + 1 < nums.length && nums[i + 1] == 7) {
+                return true;
+            }
+            if (i + 2 < nums.length && nums[i + 2] == 7) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
