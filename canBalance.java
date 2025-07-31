269398aa402c6c0b2df7f689f9511b9203ffe7d2
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-31 22:27:12 +0400

    Completed canBalance

---
+++ canBalance.java
@@ -0,0 +1,20 @@
+public boolean canBalance(int[] nums) {
+    
+    int total = 0;
+    
+    for (int num : nums) {
+        total += num;
+    }
+
+    int left = 0;
+
+    for (int i = 0; i < nums.length - 1; i++) {
+        left += nums[i];
+
+        if (left == total - left) {
+            return true;
+        }
+    }
+
+    return false;
+}
