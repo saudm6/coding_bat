33efa4bde14a1435805fa4491adbc6bae2a68ef8
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-31 20:23:17 +0400

    Completed fizzArray2

---
+++ fizzArray2.java
@@ -0,0 +1,8 @@
+public String[] fizzArray2(int n) {
+    
+    String[] result = new String[n];
+    for (int i = 0; i < n; i++) {
+        result[i] = String.valueOf(i);
+    }
+    return result;
+}
