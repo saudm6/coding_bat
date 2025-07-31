81d94d4a2b24156826b6431613c5446d53d2f953
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-31 21:43:48 +0400

    Completed zeroFront

---
+++ zeroFront.java
@@ -0,0 +1,16 @@
+public int[] zeroFront(int[] nums) {
+
+    int zero = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+
+            int temp = nums[zero];
+            nums[zero] = nums[i];
+            nums[i] = temp;
+            zero++;
+        }
+    }
+
+    return nums;
+}
