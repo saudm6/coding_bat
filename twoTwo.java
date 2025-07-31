ebf50a4f5c0f3b44334c855d86ec33229789c38a
Saud Al-Maawali <soud111mawali@gmail.com>
2025-07-31 21:18:32 +0400

    Commit twoTwo

---
+++ twoTwo.java
@@ -0,0 +1,18 @@
+public boolean twoTwo(int[] nums) {
+
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 2) {
+
+            if (i > 0 && nums[i - 1] == 2) {
+                continue;
+            }
+
+            if (i < nums.length - 1 && nums[i + 1] == 2) {
+                continue;
+            }
+
+            return false;
+        }
+    }
+    return true;
+}
