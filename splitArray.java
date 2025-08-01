50132fafa9c6437df6d683bef1af1fadb17b1047
Saud Al-Maawali <soud111mawali@gmail.com>
2025-08-01 16:52:01 +0400

    Completed splitArray

--- splitArray.java
+++ splitArray.java
@@ -1,19 +1,19 @@
 public boolean splitArray(int[] nums) {
-    return splitArrayHelper(nums, 0, 0, 0);
+    return splitArray2(nums, 0, 0, 0);
 }
 
-private boolean splitArrayHelper(int[] nums, int start, int sum1, int sum2) {
+private boolean splitArray2(int[] nums, int start, int sum1, int sum2) {
 
     if (start >= nums.length) {
         return sum1 == sum2;
     }
     int num = nums[start];
 
-    if (splitArrayHelper(nums, start + 1, sum1 + num, sum2)) {
+    if (splitArray2(nums, start + 1, sum1 + num, sum2)) {
         return true;
     }
 
-    if (splitArrayHelper(nums, start + 1, sum1, sum2 + num)) {
+    if (splitArray2(nums, start + 1, sum1, sum2 + num)) {
         return true;
     }
 
