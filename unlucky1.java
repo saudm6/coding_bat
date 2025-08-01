5966e7d93f1dc7ae9560aa4f7633906cbf242613
Saud Al-Maawali <soud111mawali@gmail.com>
2025-08-01 08:58:18 +0400

    Completed unlucky1

--- unlucky1.java
+++ unlucky1.java
@@ -1,9 +1,13 @@
 public boolean unlucky1(int[] nums) {
-    int[] arr = new int[2];
-    for (int i = 0; i < nums.length - 1; i++){
-        if (nums[i] == 1 && nums[i + 1] == 3){
+
+    for (int i = 0; i < nums.length - 1; i++) {
+
+        if ((i == 0 || i == 1 || i == nums.length - 2)
+                && nums[i] == 1
+                && nums[i + 1] == 3) {
             return true;
         }
     }
     return false;
+    
 }
