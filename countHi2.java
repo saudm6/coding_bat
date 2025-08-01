2bbbef00b309d964c426ca33532f2ac17b425857
Saud Al-Maawali <soud111mawali@gmail.com>
2025-08-01 15:32:19 +0400

    Completed countHi2

---
+++ countHi2.java
@@ -0,0 +1,15 @@
+public int countHi2(String str) {
+    if (str.length() < 2) {
+        return 0;
+    }
+
+    if (str.startsWith("xhi")) {
+        return countHi2(str.substring(3));
+    }
+
+    if (str.startsWith("hi")) {
+        return 1 + countHi2(str.substring(2));
+    }
+
+    return countHi2(str.substring(1));
+}
