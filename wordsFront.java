db2d09794587793a30dd1afe8f5a8e50b72bb1b0
Saud Al-Maawali <soud111mawali@gmail.com>
2025-08-01 09:07:41 +0400

    Completed wordsFront

---
+++ wordsFront.java
@@ -0,0 +1,9 @@
+public String[] wordsFront(String[] words, int n) {
+
+    String[] final_word = new String[n];
+
+    for (int i = 0; i < n; i++) {
+        final_word[i] = words[i];
+    }
+    return final_word;
+}
