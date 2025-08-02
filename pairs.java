51eb5bd8a74c852e580dd36abd425f5023d3ab04
Saud Al-Maawali <soud111mawali@gmail.com>
2025-08-02 11:52:19 +0400

    Completed pairs

--- pairs.java
+++ pairs.java
@@ -2,9 +2,9 @@ public Map<String, String> pairs(String[] strings) {
 
     Map<String, String> map = new HashMap<>();
     for (String string : strings) {
-        String num = string.substring(0, 1);
-        String num2 = string.substring(string.length() - 1);
-        map.put(num, num2);
+        String word = string.substring(0, 1);
+        String word2 = string.substring(string.length() - 1);
+        map.put(word, word2);
     }
     return map;
 }
